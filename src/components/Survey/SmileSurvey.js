70b5144e7a9fc754d45812daaf5143e6661e9d54
Harun Dayan <harundayann@gmail.com>
2022-03-08 06:53:42 +0300

    update-survy

--- src/components/Survey/SmileSurvey.js
+++ src/components/Survey/SmileSurvey.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import bir from "../../assets/images/bir.png";
 import iki from "../../assets/images/iki.png";
 import üç from "../../assets/images/üç.png";
